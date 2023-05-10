8c516ef9d658e9dcc9fe713794c43444b4cebf88
Freddy <79088826+freddy-noor@users.noreply.github.com>
2023-05-10 18:02:40 +0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,19 @@
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0 " + name)
+
+trip_planner_welcome("My Love")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(1.5)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
+  print("Your trip starts off in " + origin)
+  print("And you are travelling to " + destination)
+  print("You will be traveling by " + mode_of_transport)
+  print("It will take approximately " + str(estimated_time) + " hours")
+
+destination_setup("New York", "New Jersey", estimate)
+
